70d20dccaa551188ec04a0cc816fed025f0e5813
Vishal Patel <patelvishal3199@gmail.com>
2017-10-01 23:44:49 -0400

    Added JavaDoc commenting

--- src/main/java/pv3199/util/ConsumerHolder.java
+++ src/main/java/pv3199/util/ConsumerHolder.java
@@ -1,6 +1,14 @@
 package pv3199.util;
 
+/**
+ * A function that takes in a {@link ForEachHolder} instance and returns nothing.
+ */
 @FunctionalInterface
 public interface ConsumerHolder<T> {
+	/**
+	 * The void function with a {@link ForEachHolder} instance.
+	 *
+	 * @param forEachHolder the ForEachHolder instance.
+	 */
 	void accept(ForEachHolder<T> forEachHolder);
 }
